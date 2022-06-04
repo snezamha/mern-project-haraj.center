97a26e7b58f883d6194e65a524702aa8f2fac22c
snezamha <snezamha@gmail.com>
2022-06-04 04:10:32 +0200

    4Jun22

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -183,7 +183,7 @@ function ProductScreen() {
               {product.countInStock > 0 ? (
                 <Button
                   onClick={addToCartHandler}
-                  className="w-full bg-indigo-600 border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+                  className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
                   variant=""
                 >
                   {t('common.addToCard')}
--- frontend/src/screens/VerifyScreen.js
+++ frontend/src/screens/VerifyScreen.js
@@ -97,7 +97,7 @@ export default function SigninScreen() {
                 <div className="mt-2">
                   <button
                     type="submit"
-                    className="bg-primary w-full
+                    className=" w-full
 
                   bg-indigo-600 border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
                   >
